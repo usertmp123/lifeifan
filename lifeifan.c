7b8612d5f79018fa93d00af7dc60b0c3db6be95a
usertmp123 <1993552270@qq.com>
2021-11-24 20:28:03 +0800

    第二次提交test_commit

--- lifeifan.c
+++ lifeifan.c
@@ -3,6 +3,11 @@
 
 int main()
 {
+	int a = 10;
+	printf("git add lifeifan.c\n");
+	printf("git commit -m \"第二次提交库中\"\n");
+	printf("git push origin master\n");
+
 	pritnf("hello world\n");
 	return 0;
 
